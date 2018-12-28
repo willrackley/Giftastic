2ff688856c57ad83cc3923114c7d61205617d1dc
williamrackley <willrackley65@gmail.com>
2018-12-27 20:20:48 -0500

    the user can now add their own topic and view gifs

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -15,14 +15,49 @@ function showButtons(){
 function remakeButtons(){
     var inputText = $('#sportInput').val().toUpperCase().trim();
     topics.push(inputText);
-    $("#topicButtons").empty();
-   for(var i=0; i < topics.length; i++){
-    var button = $("<button>");
-    button.addClass("sportButtons");
-    button.attr("data-name", topics[i]);
-    button.text(topics[i]);
-    $("#topicButtons").append(button); 
-   }
+    $('#topicButtons').empty();
+    showButtons();
+   
+    $(".sportButtons").on("click", function() {
+        $("#gifBox").empty();
+        var topicName = $(this).attr("data-name")
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            topicName + "&api_key=17HlEsY0GKfVxvXvmi1HZw2RI94pGhFc&limit=10";
+    
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+            }).then(function(response) {
+    
+                var gifResults = response.data;
+                for (var i = 0; i < topics.length; i++) {
+    
+                    var newDiv = $("<div>");
+                    var ratingParagraph = $("<p>").text("Rating: " + gifResults[i].rating);
+                    var gifImage= $("<img>");
+                    gifImage.addClass("gif");
+                    gifImage.addClass("figure-img");
+                    gifImage.attr("src", gifResults[i].images.fixed_height_still.url);
+                    newDiv.addClass("float-left");
+                    newDiv.append(gifImage);
+                    newDiv.append(ratingParagraph);
+                    $("#gifBox").prepend(newDiv);
+                }
+                $(".gif").on("click", function() {
+                   
+                    var source = $(this).attr("src");
+                    for(var i = 0; i < gifResults.length; i++) {
+                    if(source === gifResults[i].images.fixed_height_still.url){
+                        $(this).attr("src", gifResults[i].images.fixed_height.url);
+                    }
+                    if(source === gifResults[i].images.fixed_height.url){
+                        $(this).attr("src", gifResults[i].images.fixed_height_still.url);
+                    }
+                    }
+                });
+        });    
+    });
+    $('#sportInput').val('');
 }
 
 showButtons();
@@ -39,7 +74,7 @@ $(".sportButtons").on("click", function() {
         }).then(function(response) {
 
             var gifResults = response.data;
-            for (var i = 0; i < gifResults.length; i++) {
+            for (var i = 0; i < topics.length; i++) {
 
                 var newDiv = $("<div>");
                 var ratingParagraph = $("<p>").text("Rating: " + gifResults[i].rating);
@@ -71,10 +106,7 @@ $(".sportButtons").on("click", function() {
 
 $("#submitButton").on("click", function(){
     event.preventDefault();
-    $("#gifBox").empty();
     remakeButtons();
-console.log(topics);
-
 });
 
 
